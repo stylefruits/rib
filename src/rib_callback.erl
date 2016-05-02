cd93d37d01b7e1ce30a82d59c5b8f3b788111b9d
Marek Kubica <marek.kubica@stylefruits.de>
2016-05-02 16:48:45 +0200

    Extract Cache-Control headers from subrequest and determine the minimum
    This makes sure that the Cache header returned by Rib is not longer than
    the shortest duration of the requests.

--- src/rib_callback.erl
+++ src/rib_callback.erl
@@ -43,8 +43,17 @@ handle('OPTIONS',[<<"v1">>, <<"batch">>], _Req) ->
 handle('POST',[<<"v1">>, <<"batch">>], Req) ->
     Requests = jiffy:decode(elli_request:body(Req), [return_maps]),
     {Taken, {ok, Responses}} = timer:tc(rib_fetch, fetch_all, [Requests]),
+    CacheHeaders = lists:map(fun(R) ->
+      {AllHeaders} = maps:get(headers, maps:get(response, R)),
+      case proplists:get_value(<<"cache-control">>, AllHeaders, undefined) of
+          undefined -> undefined;
+          Rest -> rib_parse:parse_cache_control(binary_to_list(Rest))
+      end
+    end, Responses),
+    Minimal = rib_parse:minimal_cache(CacheHeaders),
+    Cache = rib_parse:format_cache_control(Minimal),
     Data = #{results => Responses, time_taken => Taken / 1000},
-    encode_response(Req, Data);
+    encode_response(Req, Cache, Data);
 
 handle(_, _, _Req) ->
     {404, [], <<"Not Found">>}.
@@ -63,9 +72,10 @@ error_response(Err) ->
      [{<<"Content-type">>, <<"text/plain; charset=utf-8">>}],
      io_lib:format("~p~n", [Err])}.
 
-encode_response(Req, Data) ->
+encode_response(Req, Cache, Data) ->
     JSON = jiffy:encode(Data),
-    Headers = [{<<"Content-Type">>, <<"application/json; charset=utf-8">>}],
+    Headers = [{<<"Content-Type">>, <<"application/json; charset=utf-8">>},
+               {<<"Cache-Control">>, list_to_binary(Cache)}],
     case accepted_encoding(Req) of
         none -> {200, Headers, JSON};
         gzip -> {200,
